a386f85d934472ce8bd685917ad50662df3b2f90
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2021-10-14 22:40:12 +0200

    refactored

--- WebSocketClient/app.js
+++ WebSocketClient/app.js
@@ -1,9 +1,14 @@
 const message_history = document.getElementById('message_history')
 const send_button = document.getElementById('send_button')
 const connect_button = document.getElementById('connect_button')
+const ws_url = document.getElementById('ws_url')
 let ws = null
+const input_log = []
 
 function addMessage(message, out) {
+    if (ws !== null && out) {
+        ws.send(message)
+    }
     let p = document.createElement('p')
     p.appendChild(document.createTextNode(message))
     if (out) {
@@ -11,7 +16,9 @@ function addMessage(message, out) {
     } else {
         p.className = 'inbound msg'
     }
+    p.innerHTML = p.innerHTML.replaceAll('\n', '<br>')
     message_history.appendChild(p)
+    message_history.scrollTop = message.height
 }
 
 function addLog(message) {
@@ -19,31 +26,57 @@ function addLog(message) {
     p.appendChild(document.createTextNode(message))
     p.className = 'log_msg'
     message_history.appendChild(p)
+    message_history.scrollTop = message.height
 }
 
 document.getElementById('connect_button').addEventListener('click', e => {
     e.preventDefault()
     if (ws == null) {
         connect_button.value = 'Connecting'
+        connect_button.disabled = true
         send_button.disabled = true
+        ws_url.disabled = true
         message_history.innerHTML = ''
-        ws = new WebSocket(document.getElementById('ws_url').value)
+        ws = new WebSocket(ws_url.value)
+        addLog('Connecting...')
         ws.onopen = () => {
+            connect_button.disabled = false
             send_button.disabled = false
+            ws_url.disabled = false
             connect_button.value = 'Disconnect'
             addLog('Connected...')
         }
+        ws.onmessage = (e) => {
+            addMessage(e.data, false)
+        }
         ws.onclose = () => {
             ws = null
+            connect_button.disabled = false
             send_button.disabled = false
+            ws_url.disabled = false
             connect_button.value = 'Connect'
             addLog('Disconnected...')
         }
     } else {
+        ws.close()
     }
 })
 send_button.addEventListener('click', e => {
     e.preventDefault()
-    addMessage(e.target.parentElement.children[0].value, true)
+    let msg = e.target.parentElement.children[0].value
+    input_log.push(msg)
+    addMessage(msg.replaceAll('\\\\', '\\').replaceAll('\\n', '\n'), true)
     e.target.parentElement.children[0].value = ''
+})
+
+if (localStorage.getItem('lastIp') !== null) {
+    ws_url.value = localStorage.getItem('lastIp')
+}
+
+ws_url.addEventListener('input', e => {
+    if (e.target.value !== '' && e.target.value !== 'ws://localhost') {
+        localStorage.setItem('lastIp', e.target.value)
+    } else {
+        localStorage.removeItem('lastIp')
+    }
 })
